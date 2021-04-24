5216886523644d754b8d947d604a0f393da05b7f
Nikola Filipovski <nfilipovski@yahoo.com>
2021-04-25 00:39:21 +0200

    25.04.2021

--- Videostore.Data/DataContext.cs
+++ Videostore.Data/DataContext.cs
@@ -20,7 +20,51 @@ namespace Videostore.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            // Data seed
+            #region Admin and roles
+
+            const string ADMIN_ID = "b4280b6a-0613-4cbd-a9e6-f1701e926e73";
+            const string ROLE_ID = ADMIN_ID;
+            const string password = "Admin1234!";
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = ROLE_ID,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "b4280b6a-0613-4cbd-a9e6-f1701e926e74",
+                    Name = "Guest",
+                    NormalizedName = "GUEST"
+                }
+            );
+
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            builder.Entity<IdentityUser>().HasData(new IdentityUser
+            {
+                Id = ADMIN_ID,
+                UserName = "admin@videostore.com",
+                NormalizedUserName = "ADMIN@VIDEOSTORE.COM",
+                Email = "admin@videostore.com",
+                NormalizedEmail = "ADMIN@VIDEOSTORE.COM",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, password),
+                SecurityStamp = string.Empty,
+                ConcurrencyStamp = "c8554266-b401-4519-9aeb-a9283053fc58"
+            });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ROLE_ID,
+                UserId = ADMIN_ID
+            });
+
+            #endregion
+
+            #region Data seed
 
             builder.Entity<Studio>().HasData(
                 new Studio
@@ -39,6 +83,8 @@ namespace Videostore.Data
                     studioName = "Disney",
                 }
                 );
+
+            #endregion
 
             base.OnModelCreating(builder);
 
---
+++ Videostore/wwwroot/js/form-validation.js
@@ -0,0 +1,72 @@
+﻿$(function () {
+    $("form[name='movieCreate']").validate({
+        success: "valid",
+        onkeyup: true,
+        focusInvalid: true,
+        errorClass: "error", // adding error class on element -> define error class in css
+        highlight: function (element, errorClass) {
+            $(element).fadeOut(function () {
+                $(element).fadeIn();
+                $(element).addClass(errorClass);
+            });
+        },
+
+        // Validation rules
+
+        rules: {
+            title: {
+                required: true,
+                minlength: 4
+            },
+            runTime: {
+                required: true,
+                minlength: 2
+            },
+            genre: {
+                required: true,
+                minlength: 5
+            },
+            rating: {
+                required: false,
+                number: true,
+                digits: true,
+            },
+            description: {
+                required: false,
+                minlength: 10
+            }
+        },
+
+        // Validation error messages
+
+        messages: {
+            title: {
+                required: "Please enter movie title!",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            runTime: {
+                required: "Please enter movie runtime!",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            genre: {
+                required: "Please enter genre!",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+            rating: {
+                required: "Please enter the rating!",
+                number: "Please enter numbers only!",
+                digits: "Only digits are allowed in this field!",
+            },
+            description: {
+                required: "Please enter description!",
+                minlength: jQuery.validator.format("At least {0} characters required!")
+            },
+
+        },
+
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
+});
